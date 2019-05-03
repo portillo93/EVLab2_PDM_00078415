0f69b2a28e2291a3e551f90b351251bd747b5e43
GuillermoMorales <00011417@uca.edu.sv>
2019-05-03 17:25:01 -0600

    algo

--- app/src/main/java/com/example/evaluado2/MainActivity.kt
+++ app/src/main/java/com/example/evaluado2/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.evaluado2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , BarFragment.OnSelectOption {
+
+    override fun onAction(id: Int) {
+        var content = when (id) {
+            1 -> {
+                "Hola"
+            }
+            2 -> {
+                "Wiii"
+            }
+            3 -> {
+                "Bye"
+            }
+            else -> {
+                "Fuck!"
+            }
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_content, ContentFragment.newInstance(content))
+            .addToBackStack("Co")
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var barFragment = BarFragment.newInstace(
+            "Op1",
+            "Op2",
+            "Op3"
+        )
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_bar, barFragment)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/evaluado2/barfragment.kt
@@ -0,0 +1,82 @@
+package com.example.evaluado2
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.bar_fragment.view.*
+import kotlinx.android.synthetic.main.content_fragment.*
+import java.lang.RuntimeException
+
+val ARG_ACTION_1: String = "action_1"
+val ARG_ACTION_2: String = "action_2"
+
+class BarFragment : Fragment() {
+
+    var listener: OnSelectOption? = null
+    var action1: String? = null
+    var action2: String? = null
+
+
+    companion object {
+
+        fun newInstace(action1: String, action2: String,action3: String) =
+            BarFragment().apply {
+                arguments  = Bundle().apply {
+                    putString(ARG_ACTION_1,action1)
+                    putString(ARG_ACTION_2,action2)
+
+                }
+            }
+    }
+
+    interface OnSelectOption {
+        fun onAction(id: Int)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnSelectOption) {
+            listener = context
+        } else {
+            throw RuntimeException("Se necesita una implementación de  OnSelectOption")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.apply {
+            action1 = getString(ARG_ACTION_1)
+            action2 = getString(ARG_ACTION_2)
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?) =
+        inflater.inflate(R.layout.content_fragment,
+            container, false).apply {
+            prev.apply {
+                text = action1
+                setOnClickListener {
+                    listener?.onAction(1)
+                }
+            }
+            next.apply {
+                text = action2
+                setOnClickListener {
+                    listener?.onAction(2)
+                }
+            }
+        }
+
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+}
